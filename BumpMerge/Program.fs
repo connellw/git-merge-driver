264e059337ff9631fa30aa5a583f458509eae54a
Connell <connellsharp@gmail.com>
2020-07-08 00:24:43 +0100

    Clearer console output

--- BumpMerge/Program.fs
+++ BumpMerge/Program.fs
@@ -11,12 +11,10 @@ let readFile path =
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-
     let ancestorContents = readFile argv.[0]
     let currentContents = readFile argv.[1]
     let otherContents = readFile argv.[2]
 
-    printfn "%s %s %s" ancestorContents currentContents otherContents
+    printfn "|| %s || %s || %s ||" ancestorContents currentContents otherContents
 
     0 // return an integer exit code
