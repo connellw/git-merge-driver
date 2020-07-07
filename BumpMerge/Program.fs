87af167f3dca11ef7e3f82370f05086d61b61069
Connell <connellsharp@gmail.com>
2020-07-08 00:56:50 +0100

    Initial version parsing logic

--- BumpMerge/Program.fs
+++ BumpMerge/Program.fs
@@ -1,4 +1,5 @@
-﻿open System.IO
+﻿open System
+open System.IO
 
 let writeFile path contents =
     File.WriteAllText(path, contents)
@@ -9,12 +10,35 @@ let writeTestFile =
 let readFile path =
     File.ReadAllText(path)
 
+type SemVer = {
+    Major: int
+    Minor: int
+    Patch: int
+}
+
+let version major minor patch = {
+    Major = major
+    Minor = minor
+    Patch = patch
+}
+
+let versionArr (arr: int array) =
+    version arr.[0] arr.[1] arr.[2]
+
+let parseVersion (text: string) =
+    text.Trim().Split '.'
+    |> Array.map Int32.Parse
+    |> versionArr
+
 [<EntryPoint>]
 let main argv =
     let ancestorContents = readFile argv.[0]
     let currentContents = readFile argv.[1]
     let otherContents = readFile argv.[2]
-
     printfn "|| %s || %s || %s ||" ancestorContents currentContents otherContents
+
+    let versions = Array.map (readFile >> parseVersion) argv
+
+    printfn "||| Merged version: %s |||" "1.1.1"
 
     0 // return an integer exit code
